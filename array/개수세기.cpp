b689cd0f9972c902825c5731c152b3ebdd9e989c
kimminse3380 <mss2214@naver.com>
2022-09-05 19:53:13 +0900

    개수세기

---
+++ array/개수세기.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	int n, v,cnt;
+	scanf("%d", &n);
+	int memo[n];
+	for(int i=0; i<n; i++){
+		scanf("%d", &memo[i]);
+	}
+	
+	scanf("%d", &v);
+	for(int j=0; j<n; j++){
+		if(memo[j]==v){
+			cnt++;
+		}
+	}
+	printf("%d", cnt);
+}
